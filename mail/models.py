666ff3227fc9fec71d9e8589f5c2e205b973a780
zxl0715_Notebook <zxl0715@163.com>
2017-10-24 23:38:34 +0800

    登陆功能

--- mail/models.py
+++ mail/models.py
@@ -1,7 +1,10 @@
+from datetime import timezone
+
 from django.db import models
 
 
 # Create your models here.
+
 
 class User(models.Model):
     class Meta:
@@ -10,10 +13,13 @@ class User(models.Model):
     id = models.AutoField(max_length=11, db_column='UID', primary_key=True)
     userName = models.CharField(max_length=255, db_column='username', blank=False)
     password = models.CharField(max_length=255, db_column='password', blank=False)
-    cid = models.IntegerField(max_length=11, db_column='CID', blank=False)
+    cid = models.IntegerField(max_length=11, db_column='CID', blank=True)
+    addTime = models.DateTimeField(db_column='addTime', auto_now_add=True, editable=True)
 
 
 class mialAddress(models.Model):
     id = models.AutoField(max_length=11, db_column='ID', primary_key=True)
     mialUserNmae = models.CharField(max_length=255, db_column='mailUserName', blank=True)
     mail = models.CharField(max_length=255, db_column='mail', blank=False)
+    addTime = models.DateTimeField(db_column='addTime', auto_now_add=True, editable=True)
+    updateTime = models.DateTimeField(db_column='updateTime', auto_now=True, null=True)
--- mail/views.py
+++ mail/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
-def index(request):
+from mail import models
+from django.shortcuts import redirect
 
-    return render(request,"index.html")
+
+# Create your views here.
+
+def index(request):
+    context = {}
+    context['hello'] = 'hello world! 来自后台'
+    if request.method == "POST":
+        username = request.POST.get("inputUsername", None)
+        password = request.POST.get("inputPassword", None)
+        # print(username, password)
+        # temp = {"user": username, "pwd": password}
+        # models.User.objects.create(userName=username, password=password, cid=0)
+        isExists = models.User.objects.get(userName=username, password=password)
+        if isExists == True:
+            return redirect('/main/')
+        else:
+            return render(request, "index.html", {"status": "用户名或密码错误！"})
+
+
+        # user_list = models.User.objects.all()
+        # return render(request, "index.html", {"data": user_list})
+    return render(request, "index.html")
