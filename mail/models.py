284c3fc1e7be519e7d03d375dd232f658bcff7b9
zxl0715_Notebook <zxl0715@163.com>
2017-10-25 09:39:35 +0800

    登录到主页

--- mail/models.py
+++ mail/models.py
@@ -13,7 +13,7 @@ class User(models.Model):
     id = models.AutoField(max_length=11, db_column='UID', primary_key=True)
     userName = models.CharField(max_length=255, db_column='username', blank=False)
     password = models.CharField(max_length=255, db_column='password', blank=False)
-    cid = models.IntegerField(max_length=11, db_column='CID', blank=True)
+    cid = models.IntegerField(db_column='CID', blank=True)
     addTime = models.DateTimeField(db_column='addTime', auto_now_add=True, editable=True)
 
 
--- mail/views.py
+++ mail/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib import auth
 from mail import models
 from django.shortcuts import redirect
 
@@ -16,12 +16,11 @@ def index(request):
         # temp = {"user": username, "pwd": password}
         # models.User.objects.create(userName=username, password=password, cid=0)
         isExists = models.User.objects.get(userName=username, password=password)
-        if isExists == True:
-            return redirect('/main/')
+        # user = auth.authenticate(username=username, password=password)
+        if isExists is not None:
+            return redirect('/main.html')
         else:
             return render(request, "index.html", {"status": "用户名或密码错误！"})
-
-
-        # user_list = models.User.objects.all()
-        # return render(request, "index.html", {"data": user_list})
+            # user_list = models.User.objects.all()
+            # return render(request, "index.html", {"data": user_list})
     return render(request, "index.html")
