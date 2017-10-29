0865ce7969c1acacd6099eb0051a12e3e8806a0b
zxl0715_Notebook <zxl0715@163.com>
2017-10-29 22:26:58 +0800

    添加 写邮件模块 前端

---
+++ static/js/wirteemail.js
@@ -0,0 +1,21 @@
+//实例化编辑器
+//建议使用工厂方法getEditor创建和引用编辑器实例，如果在某个闭包下引用该编辑器，直接调用UE.getEditor('editor')就能拿到相关的实例
+var ue = UE.getEditor('editor');
+
+// 获取内容
+function getContent() {
+    var arr = [];
+    arr.push("使用editor.getContent()方法可以获得编辑器的内容");
+    arr.push("内容为：");
+    arr.push(UE.getEditor('editor').getContent());
+    alert(arr.join("\n"));
+}
+
+// 是否有值
+function hasContent() {
+    var arr = [];
+    arr.push("使用editor.hasContents()方法判断编辑器里是否有内容");
+    arr.push("判断结果为：");
+    arr.push(UE.getEditor('editor').hasContents());
+    alert(arr.join("\n"));
+}
